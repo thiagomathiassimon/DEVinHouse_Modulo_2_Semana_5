1c68c326c817be2787a1fe12a90cc189947a6e01
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-04-14 08:43:58 -0300

    Exercicio_14

---
+++ Exercicio_14/springboot/src/main/java/br/com/devinhouse/springboot/service/Paramentros.java
@@ -0,0 +1,45 @@
+package br.com.devinhouse.springboot.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Paramentros {
+
+	@Value("${pessoa.nome}")
+	private String nome;
+
+	@Value("${pessoa.idade}")
+	private int idade;
+
+	@Value("${pessoa.profissao}")
+	private String profissao;
+
+	public String getNome() {
+		System.out.println(this.nome);
+		return this.nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public int getIdade() {
+		System.out.println(this.idade);
+		return this.idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+
+	public String getProfissao() {
+		System.out.println(this.profissao);
+		return this.profissao;
+	}
+
+	public void setProfissao(String profissao) {
+		this.profissao = profissao;
+	}
+
+}
